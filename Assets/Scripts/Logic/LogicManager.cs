d47dc7b0ba8f23a6eeb4cd7ef6bdc75428544e4e
Akarin01 <1069891351@qq.com>
2025-02-23 15:25:01 +0800

    实现 LogicManager，管理创建的 Logic 实例

---
+++ Assets/Scripts/Logic/LogicManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 逻辑管理器类，利用享元模式复用逻辑类
+/// </summary>
+public static class LogicManager
+{
+    private static readonly Dictionary<Type, LogicBase> s_logicDict = new();
+
+    public static LogicBase GetLogic<T>() where T : LogicBase, new()
+    {
+        Type t = typeof(T);
+        if (s_logicDict.ContainsKey(t))
+        {
+            return s_logicDict[t];
+        }
+        // 创建新实例
+        LogicBase logic = Activator.CreateInstance<T>();
+        s_logicDict.Add(t, logic);
+        return logic;
+    }
+
+    public static LogicBase GetLogic(Type t)
+    {
+        if (!t.IsSubclassOf(typeof(LogicBase)))
+        {
+            throw new ArgumentException("Type must be subclass of LogicBase");
+        }
+        if (s_logicDict.ContainsKey(t))
+        {
+            return s_logicDict[t];
+        }
+        // 创建新实例
+        LogicBase logic = Activator.CreateInstance(t) as LogicBase;
+        s_logicDict.Add(t, logic);
+        return logic;
+    }
+}
