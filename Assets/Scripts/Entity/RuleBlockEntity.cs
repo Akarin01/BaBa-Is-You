e9f1571712a53aa9e0ae94375d85c74b88601e93
Akarin01 <1069891351@qq.com>
2025-02-23 16:59:28 +0800

    1

---
+++ Assets/Scripts/Entity/RuleBlockEntity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RuleBlockEntity : EntityBase
+{
+    public override LogicBase Logic
+    {
+        get => s_logic;
+        set
+        {
+            Debug.LogError("Rule Block can't change logic");
+        }
+    }
+
+    private static LogicBase s_logic;
+}
