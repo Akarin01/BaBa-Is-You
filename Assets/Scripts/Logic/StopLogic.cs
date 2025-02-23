f4f0fd2aa99b174414187ed2764299fc2a5c13be
Akarin01 <1069891351@qq.com>
2025-02-23 14:51:46 +0800

    实现 WallEntity 和 StopLogic

---
+++ Assets/Scripts/Logic/StopLogic.cs
@@ -0,0 +1,17 @@
+﻿public class StopLogic : LogicBase
+{
+    public override void OnFixedUpdate(EntityBase entity)
+    {
+        
+    }
+
+    public override void OnUpdate(EntityBase entity)
+    {
+        
+    }
+
+    public override bool TryInteract(EntityBase initiator, EntityBase receiver)
+    {
+        return false;
+    }
+}
