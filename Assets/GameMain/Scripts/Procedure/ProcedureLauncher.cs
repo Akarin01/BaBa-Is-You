bb1151d9f0035bea9e38199c5e313f7db38900ea
Akarin01 <1069891351@qq.com>
2025-03-05 15:09:18 +0800

    使用 Procedure 替换掉原本用于切换场景的临时脚本

--- Assets/GameMain/Scripts/Procedure/ProcedureLauncher.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureLauncher.cs
@@ -1,9 +1,17 @@
 ﻿using KitaFramework;
+using UnityEngine.SceneManagement;
 
 namespace BabaIsYou
 {
     public class ProcedureLauncher : ProcedureBase
     {
+        protected internal override void OnEnter(IFsm<ProcedureManager> procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
 
+            SceneManager.LoadScene("Menu");
+
+            ChangeState<ProcedureMenu>(procedureOwner);
+        }
     }
 }
--- Assets/GameMain/Scripts/Procedure/ProcedureMenu.cs
+++ Assets/GameMain/Scripts/Procedure/ProcedureMenu.cs
@@ -4,6 +4,11 @@ namespace BabaIsYou
 {
     public class ProcedureMenu : ProcedureBase
     {
+        protected internal override void OnEnter(IFsm<ProcedureManager> procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
 
+            GameEntry.UIManager.OpenUI<MenuForm>();
+        }
     }
 }
