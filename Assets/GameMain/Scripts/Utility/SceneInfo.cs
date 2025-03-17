5dff388842ab59e87df51c4ffffaee45150af386
Akarin01 <1069891351@qq.com>
2025-03-17 20:12:44 +0800

    使用地图编辑器制作关卡

--- Assets/GameMain/Scripts/Utility/SceneInfo.cs
+++ Assets/GameMain/Scripts/Utility/SceneInfo.cs
@@ -3,6 +3,6 @@
     public static class SceneInfo
     {
         public static int CurrentSceneId { get; set; }
-        public const int MAX_SCENE_ID = 4;
+        public const int MAX_SCENE_ID = 5;
     }
 }
